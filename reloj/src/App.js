cb66d8baae4c8973fc41f1e136515e8155b1ec15
Luisito <luisitoylag@gmail.com>
2022-12-28 06:39:27 -0500

    Movimiento de  las manecillas del Reloj en el componente Clock

--- reloj/src/App.js
+++ reloj/src/App.js
@@ -2,14 +2,7 @@ import "./App.css";
 import { Clock } from "./components/Clock";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Reloj Zona Ecuador</h1>
-        <Clock></Clock>
-      </header>
-    </div>
-  );
+  return <Clock></Clock>;
 }
 
 export default App;
--- reloj/src/components/Clock.jsx
+++ reloj/src/components/Clock.jsx
@@ -1,5 +1,63 @@
-import React from "react";
+import { Component } from "react";
 
-export const Clock = () => {
-  return <div>Clock</div>;
-};
+export class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date(),
+    };
+  }
+
+  //Mediante esto hacemos que gire cada y se ejecute cada segundo, sino en cada recargada mostrar la posicion actual
+  componentDidMount() {
+    this.timeId = setInterval(() => {
+      this.setState({
+        time: new Date(),
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timeId);
+  }
+
+  render() {
+    return (
+      <div className="clock">
+        <div
+          className="hand hour"
+          data-hour-hand
+          style={{
+            transform: `rotateZ(${this.state.time.getHours() * 30}deg)`,
+          }}
+        ></div>
+        <div
+          className="hand minute"
+          data-minute-hand
+          style={{
+            transform: `rotateZ(${this.state.time.getMinutes() * 6}deg)`,
+          }}
+        ></div>
+        <div
+          className="hand second"
+          data-second-hand
+          style={{
+            transform: `rotateZ(${this.state.time.getSeconds() * 6}deg)`,
+          }}
+        ></div>
+        <div className="number number1">1</div>
+        <div className="number number2">2</div>
+        <div className="number number3">3</div>
+        <div className="number number4">4</div>
+        <div className="number number5">5</div>
+        <div className="number number6">6</div>
+        <div className="number number7">7</div>
+        <div className="number number8">8</div>
+        <div className="number number9">9</div>
+        <div className="number number10">10</div>
+        <div className="number number11">11</div>
+        <div className="number number12">12</div>
+      </div>
+    );
+  }
+}
